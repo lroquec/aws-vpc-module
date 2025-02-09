a296d5a0838dc792412972c13bb36637b7cd64be
Laura Roque <lroquecu@gmail.com>
2025-02-09 16:38:57 +0100

    feat: Add complete example configuration with VPC, subnets, and outputs

---
+++ examples/complete/main.tf
@@ -0,0 +1,63 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+terraform {
+  required_version = ">= 1.7.0"
+}
+
+module "vpc" {
+  source       = "../../"
+  environment  = "prod"
+  project_name = "ecommerce"
+  accountable  = "platform-team"
+  git_repo     = "https://github.com/company/ecommerce-platform"
+
+  vpc_cidr = "10.100.0.0/16"
+
+  vpc_subnet_config = {
+    public1 = {
+      cidr_block = "10.100.1.0/24"
+      public     = true
+    }
+    public2 = {
+      cidr_block = "10.100.2.0/24"
+      public     = true
+    }
+    private1 = {
+      cidr_block = "10.100.10.0/24"
+    }
+    private2 = {
+      cidr_block = "10.100.11.0/24"
+    }
+  }
+
+  create_database_subnets    = true
+  create_elasticache_subnets = true
+  enable_flow_log            = true
+
+  default_security_group_ingress = [
+    {
+      from_port   = "443"
+      to_port     = "443"
+      protocol    = "tcp"
+      cidr_blocks = "10.100.0.0/16"
+      description = "Internal HTTPS"
+    }
+  ]
+
+  default_security_group_egress = [
+    {
+      from_port   = "0"
+      to_port     = "0"
+      protocol    = "-1"
+      cidr_blocks = "0.0.0.0/0"
+      description = "Allow all outbound"
+    }
+  ]
+
+  tags = {
+    Project    = "ecommerce"
+    CostCenter = "platform"
+  }
+}
---
+++ examples/complete/outputs.tf
@@ -0,0 +1,24 @@
+output "vpc_id" {
+  description = "ID of the VPC"
+  value       = module.vpc.vpc_id
+}
+
+output "public_subnets" {
+  description = "List of IDs of public subnets"
+  value       = module.vpc.public_subnets
+}
+
+output "private_subnets" {
+  description = "List of IDs of private subnets"
+  value       = module.vpc.private_subnets
+}
+
+output "database_subnets" {
+  description = "List of IDs of database subnets"
+  value       = module.vpc.database_subnets
+}
+
+output "elasticache_subnets" {
+  description = "List of IDs of elasticache subnets"
+  value       = module.vpc.elasticache_subnets
+}
