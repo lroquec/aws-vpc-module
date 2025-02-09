183a0040fd49cb6e3f0908e14c6072e2cb89de79
Laura Roque <lroquecu@gmail.com>
2025-02-09 16:29:26 +0100

    feat: Add support for database and elasticache subnets, and enable VPC Flow Logs

--- 3-outputs.tf
+++ 3-outputs.tf
@@ -12,3 +12,13 @@ output "private_subnets" {
   description = "List of IDs of private subnets"
   value       = module.vpc.private_subnets
 }
+
+output "database_subnets" {
+  description = "List of IDs of database subnets"
+  value       = module.vpc.database_subnets
+}
+
+output "elasticache_subnets" {
+  description = "List of IDs of elasticache subnets"
+  value       = module.vpc.elasticache_subnets
+}
