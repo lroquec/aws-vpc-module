881596f502990428b9de4fa919902b8af587a13c
Laura Roque <lroquecu@gmail.com>
2025-02-10 10:30:59 +0100

    chore: Standardize formatting of VPC module source in example configurations

--- examples/basic-usage/main.tf
+++ examples/basic-usage/main.tf
@@ -7,7 +7,7 @@ terraform {
 }
 
 module "vpc" {
-  source       = "git::https://github.com/lroquec/aws-vpc-module.git//?ref=v1.5.0"  # Use remote module
+  source = "git::https://github.com/lroquec/aws-vpc-module.git//?ref=v1.5.0" # Use remote module
   # source                 = "../../" # Use local module
   environment            = "dev"
   project_name           = "test-project"
