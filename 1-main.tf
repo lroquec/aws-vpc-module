ffabca84ad2281d0ecd4dfca843dd6a0c0a734b7
Laura Roque <lroquecu@gmail.com>
2025-02-12 14:42:55 +0100

    fix: Update cidr_blocks to use list syntax in security group configuration

--- 1-main.tf
+++ 1-main.tf
@@ -85,7 +85,7 @@ resource "aws_security_group" "default" {
       from_port   = ingress.key
       to_port     = ingress.key
       protocol    = "tcp"
-      cidr_blocks = ingress.value
+      cidr_blocks = [ingress.value]
     }
   }
 
